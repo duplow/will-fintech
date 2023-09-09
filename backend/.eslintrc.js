197d79bd7b690a6897217c3c8191a75aedf01b3b
duplow <william.wagner.br@gmail.com>
2023-09-09 01:59:09 -0300

    Fix lint for monorepo

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    project: ['tsconfig.json', './apps/*/tsconfig.json'],
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
@@ -15,17 +15,5 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/semi': ['warn', 'never'],
-    '@typescript-eslint/comma-dangle': ['error', 'only-multiline'],
-    'linebreak-style': ['error', 'unix'],
-    'eol-last': ["error", "always"],
-    'no-trailing-spaces': 'error',
-    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
-  },
+  ignorePatterns: ['.eslintrc.js']
 };
